63319d9f63c08eddc53407180546bf3f5b53a757
vipin rawat <rawat.vipin4818@gmail.com>
2021-01-16 10:51:50 +0530

    added food cards

--- pages/chef/index.js
+++ pages/chef/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
 import Banner from '../../src/components/Chef/Banner';
+import FoodCard from '../../src/components/FoodComp/FoodCard';
 import Layout from '../../src/layout';
 
 function ChefPage() {
   return (
     <Layout hasBanner>
       <Banner />
-      <div className="container mx-auto">s</div>
+      <div className="container mx-auto">
+        <div className="flex flex-row space-x-8 py-8 border-b">
+          <div className="text-base">Picked for you</div>
+          <div className="text-base">Small Plates</div>
+          <div className="text-base">Soup</div>
+        </div>
+        <div className="grid grid-cols-3 gap-6 py-14">
+          <FoodCard>1</FoodCard>
+          <FoodCard>1</FoodCard>
+          <FoodCard>1</FoodCard>
+          <FoodCard>1</FoodCard>
+          <FoodCard>1</FoodCard>
+          <FoodCard>1</FoodCard>
+        </div>
+      </div>
     </Layout>
   );
 }
